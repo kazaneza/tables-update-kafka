0c3502beb0ccb7293f5fbf4d24277005cc983eef
Gentil KAZANEZA <gkazaneza@bk.rw>
2024-01-03 23:32:28 +0200

    add pyspark but not working

--- python/spark/spark-to-terminal.py
+++ python/spark/spark-to-terminal.py
@@ -1,17 +1,5 @@
+import pyspark
+
 from pyspark.sql import SparkSession
-
-# Create a Spark session
-spark = SparkSession.builder.appName("PySparkTest").getOrCreate()
-
-# Simple data for testing
-data = [("Java", "20000"), ("Python", "100000"), ("Scala", "3000")]
-
-# Create DataFrame
-columns = ["Language", "Users"]
-df = spark.createDataFrame(data, columns)
-
-# Show DataFrame
-df.show()
-
-# Stop the Spark session
-spark.stop()
+spark=SparkSession.builder.appName('Practise').getOrCreate()
+spark
