5b7a432c43b9ca1a92c73e0d335ddc30887e90be
Gentil KAZANEZA <gkazaneza@bk.rw>
2024-01-05 14:51:41 +0200

    added other columns in a table

--- python/project/main.py
+++ python/project/main.py
@@ -18,18 +18,32 @@ def main():
             if msg is None or msg.error():
                 continue
 
-            json_data, processing_time, entity_name, entity_id = process_message(msg)
+            message_json, message_fields = process_message(msg)
+            entity_name = message_fields.get('entityName')
 
             if entity_name in config:
                 table_config = config[entity_name]
                 table_name = table_config["table"]
-                columns = ', '.join(table_config["columns"])
-                placeholders = ', '.join(['?'] * len(table_config["columns"]))
-                insert_query = f"INSERT INTO {table_name}({columns}) VALUES({placeholders})"
-                values = [json_data, processing_time, entity_name, entity_id]
+                # columns = ', '.join(table_config["columns"])
+                columns = table_config["columns"]
 
+                # Construct the query
+                column_names = ', '.join(columns)
+                placeholders = ', '.join(['?'] * len(columns))
+                insert_query = f"INSERT INTO {table_name}({column_names}) VALUES({placeholders})"
+
+                # Prepare the values for insertion
+                values = [message_json] + [message_fields.get(column) for column in columns[1:]]
+
+                # Execute the query
                 execute_insert_query(cursor, insert_query, *values)
                 cnxn.commit()
+                
+
+
+                # placeholders = ', '.join(['?'] * len(table_config["columns"]))
+                # insert_query = f"INSERT INTO {table_name}({columns}) VALUES({placeholders})"
+                # values = [json_data, processing_time, entity_name, entity_id]
 
             
 
